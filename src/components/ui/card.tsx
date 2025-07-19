6f952b759b56bd05fb6c8ac9f41e22c411d37bcf
Akshatt4813 <akshatshah1918@gmail.com>
2025-07-19 11:38:03 -0700

    Add files via upload

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,2 @@
+export const Card = ({ children, className = "" }: any) => <div className={`rounded-lg border ${className}`}>{children}</div>;
+export const CardContent = ({ children, className = "" }: any) => <div className={className}>{children}</div>;
